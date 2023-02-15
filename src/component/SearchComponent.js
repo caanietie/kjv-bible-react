21abe5cd27f6352b7264c55ae7abaab61b7beff0
Anietie <aakjhonclems@gmail.com>
2023-02-15 16:53:14 +0100

    Refactored SearchComponent code

--- src/component/SearchComponent.js
+++ src/component/SearchComponent.js
@@ -1,78 +1,57 @@
 import { Component, createElement } from "react";
+import FontSizeComponent from "./FontSizeComponent";
 
 export default class SearchComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { bookName: "Genesis", chapter: 1, search: "" };
-  }
-  render() {
-    return createElement(
-      "div",
-      { style: { display: "flex", justifyContent: "space-between" } },
-      createElement(
-        "form",
-        {
-          onSubmit: event => {
-            event.preventDefault();
-            this.props.submitRequest(this.state);
-          }
-        },
-        createElement(
-          "input",
-          {
-            value: this.state.bookName,
-            type: "text", placeholder: "Book",
-            className: "searchComponent_bookName",
-            onChange: event => this.setState({ bookName: event.target.value })
-          },
-          null
-        ),
-        createElement(
-          "input",
-          {
-            className: "searchComponent_chapter",
-            type: "number", placeholder: "Chapter",
-            min: 1, max: 176, value: this.state.chapter,
-            onChange: event => this.setState({ chapter: event.target.value })
-          },
-          null
-        ),
-        createElement(
-          "button",
-          {
-            type: "submit",
-            className: "searchComponent_submitRequest"
-          },
-          "Read"
-        )
-      ),
-      createElement(
-        "form",
-        {
-          onSubmit: event => {
-            event.preventDefault();
-            this.props.searchBible(this.state.search);
-          }
-        },
-        createElement(
-          "input",
-          {
-            value: this.state.search,
-            type: "text", placeholder: "Search",
-            className: "searchComponent_bookName",
-            onChange: event => this.setState({ search: event.target.value })
-          },
-          null
-        ),
-        createElement(
-          "button",
-          {
-            type: "submit",
-            className: "searchComponent_submitRequest"
-          },
-          "Search"
-        )
-      )
-    )
-  }
+	render() {
+		const { bibleUrl, changeFontSize, fontSize } = this.props;
+		const searchValue = this.props.bibleData.search.value;
+		const searchData = this.props.bibleData.search.data.map(data => {
+			const { book_name: bookName, chapter,
+				verse, verse_id: verseId, info_text: infoText } = data;
+			return createElement(
+				"tr",
+				{ key: verseId },
+				createElement(
+					"td",
+					null,
+					createElement(
+						"a",
+						{ href: `${bibleUrl}/${bookName}/${chapter}#${verse}` },
+						`${bookName} ${chapter}:${verse}`
+					)
+				),
+				createElement("td", null, `${infoText}`)
+			);
+		});
+		const searchLength = this.props.bibleData.search.data.length;
+		const searchMessage = searchLength === 0 ? "are no results" :
+			searchLength === 1 ? "is 1 result" :
+				`are ${searchLength} results`;
+
+		return createElement(
+			"div",
+			{ style: { fontSize } },
+			createElement(
+				"div",
+				{
+					style: {
+						display: "flex", marginRight: 50,
+						justifyContent: "space-between"
+					}
+				},
+				createElement(
+					"div",
+					null,
+					`There ${searchMessage} for `,
+					createElement("b", null, `${searchValue}`),
+				),
+				createElement(FontSizeComponent, { changeFontSize }, null),
+			),
+			createElement(
+				"table",
+				null,
+				createElement("tbody", null, searchData)
+			)
+		)
+	}
 }
