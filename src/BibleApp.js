5126dc350d3d60c9733cf5dbd68db834335cecb0
Anietie <aakjhonclems@gmail.com>
2023-02-15 16:21:10 +0100

    Refactored this file to accomodate for reading and searching for text in the bible.

--- src/BibleApp.js
+++ src/BibleApp.js
@@ -1,78 +1,82 @@
 import "./App.css";
 import { Component, createElement } from "react";
+import FormComponent from "./component/FormComponent";
 import SearchComponent from "./component/SearchComponent";
 import FooterComponent from "./component/FooterComponent";
 import ChapterComponent from "./component/ChapterComponent";
 
 export default class BibleApp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: 1, book_id: 1, bookName: "Genesis", chapter: 1,
-      infoHTML: "", previousChapter: {}, nextChapter: {}
-    };
-  }
-  render() {
-    return createElement(
-      "div",
-      null,
-      createElement(
-        SearchComponent,
-        {
-          submitRequest: book => this.submitRequest(book),
-          searchBible: searchVal => this.searchBible(searchVal)
-        },
-        null
-      ),
-      createElement(
-        ChapterComponent,
-        {
-          bookName: this.state.bookName,
-          chapter: this.state.chapter,
-          infoHTML: this.state.infoHTML,
-          prevNextProps: {
-            nextProps: {
-              bookName: this.state.nextChapter.book_name,
-              chapter: this.state.nextChapter.chapter,
-            },
-            previousProps: {
-              bookName: this.state.previousChapter.book_name,
-              chapter: this.state.previousChapter.chapter,
-            },
-            requestChapter: book => this.submitRequest(book)
-          },
-        },
-        null
-      ),
-      createElement(
-        FooterComponent,
-        null,
-        null
-      )
-    )
-  }
-  searchBible(searchVal) {
-    if (!searchVal.trim()) return;
-    let msg = "Traverse the wordlist database and display results";
-    msg += `for ${searchVal}`
-    alert(msg)
-  }
-  submitRequest({ bookName, chapter }) {
-    const addr = "http://192.168.43.69:3000/kjv_bible/api/kjv_bible";
-    fetch(`${addr}/${bookName}/${chapter}`)
-      .then(response => response.json())
-      .then(response => this.setState({
-        id: response.id,
-        book_id: response.book_id,
-        bookName: response.book_name,
-        chapter: response.chapter,
-        infoHTML: response.info_html,
-        nextChapter: response.next_chapter,
-        previousChapter: response.previous_chapter
-      }))
-      .catch(error => { alert(error.message) })
-  }
-  componentDidMount() {
-    this.submitRequest(this.state);
-  }
+    constructor(props) {
+        super(props);
+        this.state = { bibleData: {}, isSearch: false, fontSize: 20 };
+        this.bibleUrl = "http://localhost:3000/kjv_bible/api/kjv_bible";
+    }
+
+    render() {
+        let bibleBody;
+        if (this.state.isSearch) bibleBody = createElement(
+            SearchComponent,
+            {
+                bibleUrl: this.bibleUrl,
+                fontSize: this.state.fontSize,
+                bibleData: this.state.bibleData,
+                changeFontSize: step => this.changeFontSize(step)
+            }, null
+        )
+        else bibleBody = createElement(
+            ChapterComponent,
+            {
+                fontSize: this.state.fontSize,
+                bibleData: this.state.bibleData,
+                changeFontSize: step => this.changeFontSize(step),
+                requestChapter: (bk, chp) => this.submitRequest(bk, chp)
+            }, null
+        )
+        return createElement(
+            "div",
+            null,
+            createElement(
+                FormComponent,
+                {
+                    searchBible: word => this.searchBible(word),
+                    requestChapter: (bk, chp) => this.submitRequest(bk, chp)
+                }, null
+            ),
+            bibleBody,
+            createElement(FooterComponent, null, null)
+        )
+    }
+
+    componentDidMount() {
+        this.submitRequest("Genesis", 1);
+    }
+
+    submitRequest(bookName, chapter) {
+        if (bookName.trim() && chapter)
+            fetch(`${this.bibleUrl}/${encodeURIComponent(bookName)}/${chapter}`)
+                .then(response => response.json())
+                .then(response => this.setState({
+                    isSearch: false,
+                    bibleData: response
+                }))
+                .catch(error => { alert(error.message) })
+    }
+
+    searchBible(searchVal) {
+        if (searchVal)
+            fetch(`${this.bibleUrl}/search/${encodeURIComponent(searchVal)}`)
+                .then(response => response.json())
+                .then(response => this.setState({
+                    isSearch: true,
+                    bibleData: response
+                }))
+                .catch(error => { alert(error.message) })
+    }
+
+    changeFontSize(step) {
+        if (step)
+            this.setState({ fontSize: this.state.fontSize + 1 })
+        else if (this.state.fontSize > 15)
+            this.setState({ fontSize: this.state.fontSize - 1 })
+    }
 }
