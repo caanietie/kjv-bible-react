b8e433419dd1f9034e8743ca2002c17601389009
Anietie <aakjhonclems@gmail.com>
2023-02-15 16:52:07 +0100

    Changed the tab size

--- src/component/FontSizeComponent.js
+++ src/component/FontSizeComponent.js
@@ -3,30 +3,30 @@ import zoomIn from "../assets/zoom-in.png";
 import zoomOut from "../assets/zoom-out.png";
 
 export default class FontSizeComponent extends Component {
-  render() {
-    return createElement(
-      "div",
-      { style: { display: "flex", gap: 10, alignSelf: "center" } },
-      createElement(
-        "img",
-        {
-          alt: "Increase font size",
-          src: zoomIn, width: 22, height: 22,
-          className: "chapterComponent__zoom",
-          onClick: () => this.props.changeFontSize(true)
-        },
-        null
-      ),
-      createElement(
-        "img",
-        {
-          alt: "Decrease font size",
-          src: zoomOut, width: 22, height: 22,
-          className: "chapterComponent__zoom",
-          onClick: () => this.props.changeFontSize(false)
-        },
-        null
-      )
-    )
-  }
+    render() {
+        const { changeFontSize } = this.props;
+
+        return createElement(
+            "div",
+            { style: { display: "flex", gap: 10, alignSelf: "center" } },
+            createElement(
+                "img",
+                {
+                    alt: "Increase font size",
+                    src: zoomIn, width: 22, height: 22,
+                    className: "chapterComponent__zoom",
+                    onClick: () => changeFontSize(true)
+                }, null
+            ),
+            createElement(
+                "img",
+                {
+                    alt: "Decrease font size",
+                    src: zoomOut, width: 22, height: 22,
+                    className: "chapterComponent__zoom",
+                    onClick: () => changeFontSize(false)
+                }, null
+            )
+        )
+    }
 }
--- src/component/PrevNextComponent.js
+++ src/component/PrevNextComponent.js
@@ -1,55 +1,56 @@
 import { Component, createElement } from "react";
 
 export default class PrevNextComponent extends Component {
-  render() {
-    const { bookName: prevBookName,
-      chapter: prevChapter } = this.props.previousProps;
-    const { bookName: nextBookName,
-      chapter: nextBookChapter } = this.props.nextProps;
-    const { requestChapter } = this.props;
-    return createElement(
-      "div",
-      { className: "prevNextComponent" },
-      createElement(
-        "div",
-        { className: "prevNextComponent_prevNext" },
-        createElement(
-          "button",
-          {
-            onClick: event => {
-              requestChapter(this.props.previousProps);
-              event.target.parentElement.parentElement
-                .parentElement.scrollIntoView()
-            }
-          },
-          `${prevBookName} ${prevChapter}`,
-        ),
-        createElement(
-          "span",
-          null,
-          "Previous Chapter"
+    render() {
+        const { bookName: prevBookName,
+            chapter: prevChapter } = this.props.previousProps;
+        const { bookName: nextBookName,
+            chapter: nextChapter } = this.props.nextProps;
+        const { requestChapter } = this.props;
+
+        return createElement(
+            "div",
+            { className: "prevNextComponent" },
+            createElement(
+                "div",
+                { className: "prevNextComponent_prevNext" },
+                createElement(
+                    "button",
+                    {
+                        onClick: event => {
+                            requestChapter(prevBookName, prevChapter);
+                            event.target.parentElement.parentElement
+                                .parentElement.scrollIntoView()
+                        }
+                    },
+                    `${prevBookName} ${prevChapter}`,
+                ),
+                createElement(
+                    "span",
+                    null,
+                    "Previous Chapter"
+                )
+            ),
+            createElement(
+                "div",
+                { className: "prevNextComponent_prevNext" },
+                createElement(
+                    "button",
+                    {
+                        onClick: event => {
+                            requestChapter(nextBookName, nextChapter);
+                            event.target.parentElement.parentElement
+                                .parentElement.scrollIntoView()
+                        }
+                    },
+                    `${nextBookName} ${nextChapter}`,
+                ),
+                createElement(
+                    "span",
+                    null,
+                    "Next Chapter"
+                )
+            )
         )
-      ),
-      createElement(
-        "div",
-        { className: "prevNextComponent_prevNext" },
-        createElement(
-          "button",
-          {
-            onClick: event => {
-              requestChapter(this.props.nextProps);
-              event.target.parentElement.parentElement
-                .parentElement.scrollIntoView()
-            }
-          },
-          `${nextBookName} ${nextBookChapter}`,
-        ),
-        createElement(
-          "span",
-          null,
-          "Next Chapter"
-        )
-      )
-    )
-  }
+    }
 }
